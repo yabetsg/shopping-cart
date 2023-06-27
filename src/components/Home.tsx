9c4d38c728d732c1c5ef7e0ee817e59187c56618
yabets <yabets.g2@gmail.com>
2023-06-27 18:49:54 -0400

    Refactor code

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,36 +1,36 @@
 import { useContext, useEffect, useState } from "react";
-import { Item } from "./Item"
-import { Nav } from "./Nav"
+import { Item } from "./Item";
+import { Nav } from "./Nav";
 import { Context } from "./Products";
-import homeBg from '../imgs/home-bg.png'
-import bg from '../imgs/bgg.png'
+import bg from "../imgs/bg.png";
 import { Link } from "react-router-dom";
-export const Home =  ()=>{
-    const [data,setData] = useState<Array<any>>([]);
-    const count = useContext(Context);
-    useEffect(()=>{
-        fetch('https://fakestoreapi.com/products')
-        .then(res=>res.json()).then((json)=>{
-         setData(json)
+export const Home = () => {
+  const [data, setData] = useState<Array<any>>([]);
+  const count = useContext(Context);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => {
+        setData(json);
+      });
+  }, []);
 
-    }
-    )
-    },[])
-    
-    
-    return(
-        <>
-        <div className="flex justify-around m-6 bg-[rgb(244,246,245)] rounded-2xl">
-
-       
-          <div className="flex flex-col items-center justify-center max-w-md gap-5">
-             <h1 className="font-['TTNormsBlack','ui-serif','Georgia'] text-8xl max-w-sm">LET'S EXPLORE UNIQUE CLOTHES.</h1>
-             <p className="font-serif">Live for influencial and innovative fashion.</p>
-             <button className="w-56 h-12 p-2 text-lg font-bold text-white bg-black rounded-3xl hover:scale-110"><Link to={'/products'}>Shop Now</Link></button>
-            </div>  
-            <img alt="" src={bg} className="w-[450px] h-[600px] shrink-0 "></img>
+  return (
+    <>
+      <div className="flex justify-around m-6 bg-[rgb(244,246,245)] rounded-2xl">
+        <div className="flex flex-col items-center justify-center max-w-md gap-5">
+          <h1 className="font-['TTNormsBlack','ui-serif','Georgia'] text-8xl max-w-sm">
+            LET'S EXPLORE UNIQUE CLOTHES.
+          </h1>
+          <p className="font-serif">
+            Live for influencial and innovative fashion.
+          </p>
+          <button className="w-56 h-12 p-2 text-lg font-bold text-white bg-black rounded-3xl hover:scale-110">
+            <Link to={"/products"}>Shop Now</Link>
+          </button>
         </div>
-        
-        </>
-    )
-}
+        <img alt="" src={bg} className="w-[450px] h-[600px] shrink-0 "></img>
+      </div>
+    </>
+  );
+};
--- src/components/RouteSwitch.tsx
+++ src/components/RouteSwitch.tsx
@@ -1,15 +1,13 @@
-import React, { MutableRefObject, useCallback, useContext, useEffect, useRef, useState } from "react";
+import React, { MutableRefObject, useCallback, useContext, useEffect, useRef, useState,MouseEvent } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./Home";
 import { Nav } from "./Nav";
 import { Context, Products } from "./Products";
 import { Cart } from "./Cart";
 import { CartView } from "./CartView";
-import uniqid from 'uniqid';
-// when clicking + simulate "add to cart click"
-// import { Products } from "./Products";
-//  export const Context = React.createContext('');
+
 export const RouteSwitch = () => {
+
   const [itemCount, setItemCount] = useState("0");
   const [cartViewStorage, setCartViewStorage] = useState<React.ReactNode[]>([]);
   const [totalPrice, setTotalPrice] = useState<number>(0);
@@ -22,66 +20,14 @@ export const RouteSwitch = () => {
   
 
   const handleClick = (e: any):void => {
-     let id:any;
-     let [itemSrc,_, itemTitle, itemPrice] = e.target.parentNode.childNodes;
-    if(e.target.getAttribute('data-products')){
-      id = e.target.id;
-  
-      
-      [itemSrc, itemTitle, itemPrice] = e.target.getAttribute('data-products').split(',')
-      let flag =false; 
-      
-      cartViewStorage.forEach(value=>{
-      
-       
-        if(typeof value === 'object' && value !== null && 'props' in value && value.props.id==id){ 
-          //value is the old item
-          //inside this if statement because the item is already in cart
-          //if item is already in cart then replace the old item with new one (updated itemCount#)
-          //       -  increment = value.props.itemCount +1
-          const incrementedItemCount = (parseInt(value.props.itemCount) + 1).toString();
-         
-          const newCartItem = (
-            <CartView
-            id={id}
-            //  handleDecrease={}
-            products={[value.props.itemSrc,value.props.itemTitle,value.props.itemPrice]}
-              handleIncrease={handleClick}
-              itemSrc={value.props.itemSrc}
-              itemTitle={value.props.itemTitle}
-              itemPrice={value.props.itemPrice}
-              itemCount={incrementedItemCount}
-            ></CartView>
-          );
-          cartViewStorage.splice(cartViewStorage.indexOf(value),1,newCartItem);
-          setCartViewStorage(cartViewStorage);
-            setItemCount((parseInt(itemCount)+1).toString());
-            setTotalPrice(totalPrice+parseFloat(itemPrice.replace('$','')));
-  
-          render?setRender(false):setRender(true);
-          
-         
-          
-            flag = true;
-            
-        }
-        
-      })
-     
-       return;
-    }else{
-      [itemSrc,_, itemTitle, itemPrice] = e.target.parentNode.childNodes;
-       id = e.target.id;
-    }
+      const [itemSrc,_, itemTitle, itemPrice] = e.target.parentNode.childNodes;
+      const id = e.target.id;
+    
    
-    let flag =false;
+     let flag =false;
     cartViewStorage.forEach(value=>{
      
       if(typeof value === 'object' && value !== null && 'props' in value && value.props.id==id){ 
-        //value is the old item
-        //inside this if statement because the item is already in cart
-        //if item is already in cart then replace the old item with new one (updated itemCount#)
-        //       -  increment = value.props.itemCount +1
         const incrementedItemCount = (parseInt(value.props.itemCount) + 1).toString();
        
         const newCartItem = (
@@ -99,7 +45,8 @@ export const RouteSwitch = () => {
         cartViewStorage.splice(cartViewStorage.indexOf(value),1,newCartItem);
         setCartViewStorage(cartViewStorage);
           setItemCount((parseInt(itemCount)+1).toString());
-          setTotalPrice(totalPrice+parseFloat(itemPrice.textContent.replace('$','')));
+          const stringToNum = parseFloat(itemPrice.textContent.replace('$',''));
+          setTotalPrice(parseFloat((totalPrice+stringToNum).toFixed(2)));
 
         render?setRender(false):setRender(true);
         
@@ -111,8 +58,8 @@ export const RouteSwitch = () => {
     if(flag) return;
     // -------------- ITEM NOT IN CART ---------------------------
     
-    
-    setTotalPrice(totalPrice+parseFloat(itemPrice.textContent.replace('$','')));
+    const stringToNum = parseFloat(itemPrice.textContent.replace('$',''));
+    setTotalPrice(parseFloat((totalPrice+stringToNum).toFixed(2)));
     let newCartItem = (
       <CartView
       id={id}
@@ -134,43 +81,11 @@ export const RouteSwitch = () => {
     setItemCount((parseInt(itemCount) + 1).toString());
     setId(id+1);
   };
-  const handleIncrease = ((e:any) =>{
-    const id = e.target?.id;
-    cartViewStorage.forEach(value=>{
-     
-      
-      if(typeof value === 'object' && value !== null && 'props' in value && value.props.id==id){ 
-        console.log('inside if');
-        setIncrementedItemCount((parseInt(value.props.itemCount) + 1).toString());
-        const incrementedItemCount = (parseInt(value.props.itemCount) + 1).toString();
-       
-        const newCartItem = (
-          <CartView
-          id={id}
-          //  handleDecrease={}
-          products={[value.props.itemSrc,value.props.itemTitle,value.props.itemPrice]}
-            handleIncrease={handleClick}
-            itemSrc={value.props.itemSrc}
-            itemTitle={value.props.itemTitle}
-            itemPrice={value.props.itemPrice}
-            itemCount={incrementedItemCount}
-          ></CartView>
-        );
-        cartViewStorage.splice(cartViewStorage.indexOf(value),1,newCartItem);
-        setCartViewStorage(cartViewStorage);
-          console.log(render);
-          
-        render?setRender(false):setRender(true);  
-      }
-    })
-    
-  });
-  // useEffect(()=>{},[handleClick])
+  
+
  useEffect(()=>{
 
-  
-  console.log('render change');
-  
+
  },[render])
  
 
